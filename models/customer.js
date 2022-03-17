6b53ce23b10f0a5dc4eecda090fd75e574a25fc4
Abdullah Omar <fishilike.fish@gmail.com>
2022-03-18 02:34:46 +0600

    feat: :file_folder: schema set up

---
+++ models/customer.js
@@ -0,0 +1,53 @@
+const { Model } = require('objection');
+const Order = require('./order');
+
+class Customer extends Model {
+	static get tableName() {
+		return 'customers';
+	}
+
+	$beforeInsert() {
+		this.createdAt = new Date();
+	}
+
+	$beforeUpdate() {
+		this.updatedAt = new Date();
+	}
+
+	static get nameColumn() {
+		return 'name';
+	}
+
+	static get emailColumn() {
+		return 'email';
+	}
+
+	static get jsonSchema() {
+		return {
+			type: 'object',
+			required: ['name', 'email'],
+			properties: {
+				id: { type: 'integer' },
+				name: { type: 'string', minLength: 1, maxLength: 255 },
+				email: { type: 'string' },
+				createdAt: { type: 'string' },
+				updatedAt: { type: 'string' },
+			},
+		};
+	}
+
+	// I guess this defines how other tables are related to this table
+	// Like an Order can only have one Customer
+	static relationMappings = {
+		order: {
+			relation: Model.HasOneRelation,
+			modelClass: Order,
+			join: {
+				from: 'customers.id',
+				to: 'orders.customer_id',
+			},
+		},
+	};
+}
+
+module.exports = Customer;
