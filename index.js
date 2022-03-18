33946498fdfc86f5554a4ba5fe57fec8884765e4
Abdullah Omar <fishilike.fish@gmail.com>
2022-03-19 01:21:54 +0600

    fix: connect to local sql

---
+++ index.js
@@ -0,0 +1,38 @@
+const Customer = require('./models/customer');
+const Order = require('./models/order');
+const knex = require('./util/database');
+
+async function main() {
+	// Delete all persons from the db.
+	await Customer.query().delete();
+	await Order.query().delete();
+
+	// Insert one row to the database.
+	const customer = await Customer.query().insert({
+		name: 'Rachel Green',
+		email: 'rg@gmail.com',
+	});
+
+	// Read all rows from the db.
+	const customerRead = await Customer.query();
+	console.log(customerRead);
+
+	const order = await Customer.relatedQuery('order')
+		.for(customer.id)
+		.insert({ total: 55 });
+	console.log(order);
+
+	const orderTotal = await Order.query()
+		.select('total')
+		.where('customer_id', '=', customer.id)
+		.orderBy('total');
+
+	console.log(orderTotal);
+}
+
+main()
+	.then(() => knex.destroy())
+	.catch((err) => {
+		console.error(err);
+		return knex.destroy();
+	});
--- util/database.js
+++ util/database.js
@@ -2,13 +2,12 @@ const { Model } = require('objection');
 const Knex = require('knex');
 
 const knex = Knex({
-	client: 'pg',
+	client: 'mysql',
 	connection: {
-		host: 'ec2-54-158-26-89.compute-1.amazonaws.com',
-		user: 'jrkuwxpxpawrhn',
-		port: '5432',
-		password: 'f6cece2840b7f0f76f0871de7bb5d16e317e4acb7538435e8ecf3696ddf7cbc0',
-		database: 'duosp0pt34khm',
+		host: '127.0.0.1',
+		user: 'root', // replace with your mysql username
+		password: '', // replace with your mysql password
+		database: 'ks-store', // replace with your db name
 	},
 });
 
